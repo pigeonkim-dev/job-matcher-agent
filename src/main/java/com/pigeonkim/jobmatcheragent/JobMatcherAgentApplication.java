accc46370630eafcc7c8e78b616e4e162b377b64
redtiger2012 <rlatmdrl86@gmail.com>
2026-05-27 09:55:31 +0900

    chore: Spring Boot 초기 셋업 + PostgreSQL 연결

---
+++ src/main/java/com/pigeonkim/jobmatcheragent/JobMatcherAgentApplication.java
@@ -0,0 +1,13 @@
+package com.pigeonkim.jobmatcheragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatcherAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatcherAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigeonkim/jobmatcheragent/JobMatcherAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeonkim.jobmatcheragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatcherAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
